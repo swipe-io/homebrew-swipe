036ce634ea20235d2b1778498109e74c59630eae
vitaly <vitaly@lobchuk.ru>
2020-11-03 08:18:09 +0300

    chore(release): 2.0.0-alpha.17

--- swipe.rb
+++ swipe.rb
@@ -2,17 +2,17 @@ require 'rbconfig'
 class Swipe < Formula
   desc ""
   homepage "https://github.com/swipe-io/swipe"
-  version "2.0.0-alpha.16"
+  version "2.0.0-alpha.17"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-alpha.16/swipe_2.0.0-alpha.16_darwin_amd64.zip"
-      sha256 "c487db005c1d3404057ab7b8fe1533fcc23b76b46154408045b74ed5f3827feb"
+      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-alpha.17/swipe_2.0.0-alpha.17_darwin_amd64.zip"
+      sha256 "2c5e114efc748a90098d6c6564c520bf21f09357eea78ec0b424c05263d4875c"
     when /linux/
-      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-alpha.16/"
+      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-alpha.17/"
       sha256 ""
     when /solaris|bsd/
       :unix
@@ -24,11 +24,11 @@ class Swipe < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-alpha.16/swipe_2.0.0-alpha.16_darwin_386.zip"
-      sha256 "db6a32e6d6750dceccc9158e7963ff96490bd56dec233868bddff39dc276c8e5"
+      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-alpha.17/swipe_2.0.0-alpha.17_darwin_386.zip"
+      sha256 "7bbdb178ece0850b31d740c6bd8ab2851661e1a4b54392debca39d58b5595e0a"
     when /linux/
-      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-alpha.16/swipe_2.0.0-alpha.16_linux_386.tar.gz"
-      sha256 "bef4ec2583e4576dd97a082be383c31aa4c118a4436aff039d09556f855a7127"
+      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-alpha.17/swipe_2.0.0-alpha.17_linux_386.tar.gz"
+      sha256 "70803327367f3fa890b795812957a27ae232cc0a18ca24ff86f5e3aa6986e613"
     when /solaris|bsd/
       :unix
     else
