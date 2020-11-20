e4686861a29f6f280fd4ca69f4bc0c08650b1c42
vitaly <vitaly@lobchuk.ru>
2020-11-20 11:36:45 +0300

    chore(release): 2.0.0-beta3

--- swipe.rb
+++ swipe.rb
@@ -2,17 +2,17 @@ require 'rbconfig'
 class Swipe < Formula
   desc ""
   homepage "https://github.com/swipe-io/swipe"
-  version "2.0.0-beta2"
+  version "2.0.0-beta3"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-beta2/swipe_2.0.0-beta2_darwin_amd64.zip"
-      sha256 "3fac4ebf2b7d2aef4e396a3bca7a23fc9fc44ebf808115de1ea3833cfd586d18"
+      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-beta3/swipe_2.0.0-beta3_darwin_amd64.zip"
+      sha256 "de05f4439ee8e7f84d67ff5fc48b51e86d11b9f47f3a483264ba6e0d9cf546e9"
     when /linux/
-      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-beta2/"
+      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-beta3/"
       sha256 ""
     when /solaris|bsd/
       :unix
@@ -24,11 +24,11 @@ class Swipe < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-beta2/swipe_2.0.0-beta2_darwin_386.zip"
-      sha256 "ed02fdf571e79df249caf560a63a9b6f1ad303f775e50fc3bb618e2a5aded2a8"
+      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-beta3/swipe_2.0.0-beta3_darwin_386.zip"
+      sha256 "b655d36717487da11522ecf127509a4109897a40aab5e1ee94d1cc9c247025e9"
     when /linux/
-      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-beta2/swipe_2.0.0-beta2_linux_386.tar.gz"
-      sha256 "381747d6575a9ecec1e99e7af8900502e8effed5e941fa2e2ec524005f7cd163"
+      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-beta3/swipe_2.0.0-beta3_linux_386.tar.gz"
+      sha256 "7a3e212d2fdc2d7098834bb4eed3c7e76992e5c3e244fd0d91902abcbd535d3a"
     when /solaris|bsd/
       :unix
     else
