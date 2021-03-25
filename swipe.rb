70ff8477342334aab131f71afeb4a2bc5bd10e2b
Vitaly Lobchuk <vitaly@lobchuk.ru>
2021-03-25 13:45:20 +0300

    chore(release): 2.0.0-rc11

--- swipe.rb
+++ swipe.rb
@@ -2,17 +2,17 @@ require 'rbconfig'
 class Swipe < Formula
   desc ""
   homepage "https://github.com/swipe-io/swipe"
-  version "2.0.0-rc10"
+  version "2.0.0-rc11"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-rc10/v2_2.0.0-rc10_darwin_amd64.zip"
-      sha256 "e29863a6f766d25927c9889f21afce37ab8c892a9c733ea3c8467c13d1588046"
+      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-rc11/v2_2.0.0-rc11_darwin_amd64.zip"
+      sha256 "4e9bb6cccae987b88d0c1c17405725fcfb25b00447ceb77f0b67f03328412904"
     when /linux/
-      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-rc10/"
+      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-rc11/"
       sha256 ""
     when /solaris|bsd/
       :unix
@@ -24,11 +24,11 @@ class Swipe < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-rc10/"
+      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-rc11/"
       sha256 ""
     when /linux/
-      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-rc10/v2_2.0.0-rc10_linux_386.tar.gz"
-      sha256 "a43cc4a78fcf5c022f336c4354eac69608c2cce971d9af4b04fe165f37424305"
+      url "https://github.com/swipe-io/swipe/releases/download/v2.0.0-rc11/v2_2.0.0-rc11_linux_386.tar.gz"
+      sha256 "b60bcf859fe1c0657f73745c9d4e7d62a8384092dbc7567af7bacac75d80d6df"
     when /solaris|bsd/
       :unix
     else
